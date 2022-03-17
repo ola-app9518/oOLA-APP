cd480b0b074e22f5bd05e1366c203ce80d6aad6b
SMS Soft <kumarsaumyakantajena@gmail.com>
2022-03-17 22:28:52 +0000

    update

--- src/main/app.java
+++ src/main/app.java
@@ -7,7 +7,7 @@ p v mymethod() {
 s.o.p("my method1.0") 
 } 
 } 
-s.op( sms soft -ola-app)  --- test1 stash comand ---
+s.op( sms soft -ola-app)  --- test2 stash comand ---
 
 S.o.p(" my method." brach ) ;
 }
